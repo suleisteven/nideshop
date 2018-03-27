f315b78321feb7eb2af652005f1b8e498e850f06
stevensl <592672849@qq.com>
2018-03-27 22:26:44 +0800

    完成反馈功能

--- src/api/controller/feedback.js
+++ src/api/controller/feedback.js
@@ -7,15 +7,26 @@ module.exports = class extends Base {
    */
   async indexAction() {
 
+    let userId = this.getLoginUserId();
+
     const feedbackData = {
       user_name: "",
       user_email: this.post('mobile'),
       msg_title: "",
       msg_type: this.post('index'),
       msg_content: this.post('content'),
-      user_id: this.getLoginUserId(),
-      msg_time : tihs.getTime(),
+      user_id: userId,
+      msg_time : this.getTime(),
     };
+
+    if(userId>0)
+    {
+      const userInfo = await this.model('user').where({id: userId}).find();
+      if(userInfo)
+      {
+        feedbackData.user_name = userInfo.nickname;
+      }
+    }
 
     await this.model('feedback').add(feedbackData);
     return this.success();
