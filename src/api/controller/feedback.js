09eec37c9cd5c17ca24c0e6194a64cb428ae987d
592672849@qq.com <592672849@qq.com>
2018-03-27 17:33:34 +0800

    修改用户反馈

--- src/api/controller/feedback.js
+++ src/api/controller/feedback.js
@@ -7,35 +7,18 @@ module.exports = class extends Base {
    */
   async indexAction() {
 
-    // TODO
-    let feedbackData = this.post();
-
-    const addressData = {
-      name: this.post('name'),
-      mobile: this.post('mobile'),
-      province_id: this.post('province_id'),
-      city_id: this.post('city_id'),
-      district_id: this.post('district_id'),
-      address: this.post('address'),
+    const feedbackData = {
+      user_name: "",
+      user_email: this.post('mobile'),
+      msg_title: "",
+      msg_type: this.post('index'),
+      msg_content: this.post('content'),
       user_id: this.getLoginUserId(),
-      is_default: this.post('is_default') === true ? 1 : 0
+      msg_time : tihs.getTime(),
     };
 
-    if (think.isEmpty(addressId)) {
-      addressId = await this.model('address').add(addressData);
-    } else {
-      await this.model('address').where({id: addressId, user_id: think.userId}).update(addressData);
-    }
-
-    // 如果设置为默认，则取消其它的默认
-    if (this.post('is_default') === true) {
-      await this.model('address').where({id: ['<>', addressId], user_id: think.userId}).update({
-        is_default: 0
-      });
-    }
-    const addressInfo = await this.model('address').where({id: addressId}).find();
-
-    return this.success(addressInfo);
+    await this.model('feedback').add(feedbackData);
+    return this.success();
   }
 
  
