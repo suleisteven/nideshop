4a3c356420d7ec333b389bc3c14d2f2547767b88
stevensl <592672849@qq.com>
2018-04-01 22:24:19 +0800

    完成后台发货功能

--- src/admin/controller/order.js
+++ src/admin/controller/order.js
@@ -22,6 +22,12 @@ module.exports = class extends Base {
     return this.success(data);
   }
 
+  async shipperInfosAction()
+  {
+      let shipperInfos = await this.model('shipper').select();
+      return this.success(shipperInfos);
+  }
+
   async infoAction() {
     const id = this.get('id');
     const model = this.model('order');
@@ -47,6 +53,9 @@ module.exports = class extends Base {
 
     const goodsInfos = await this.model('order_goods').field('nideshop_goods.*').join('nideshop_goods ON nideshop_order_goods.goods_id = nideshop_goods.id').where({'nideshop_order_goods.order_id': id}).select();
     orderInfo.goodsInfos = goodsInfos;
+
+    const expressInfo = await this.model('order_express').where({order_id: id}).find();
+    orderInfo.expressInfo = expressInfo;
     // const data = await model.where({id: id}).find();
 
     return this.success(orderInfo);
@@ -70,7 +79,46 @@ module.exports = class extends Base {
 
     const model = this.model('order');
 
-    if (id > 0 && !think.isEmpty(values.order_status)) {
+    if (id > 0 && values.order_status !=null) {
+
+      if(!think.isEmpty(values.expressInfo))
+      {
+        if(!think.isEmpty(values.expressInfo.shipper_id))
+        {
+          let shipperInfo = await this.model('shipper').where({id:values.expressInfo.shipper_id}).find();
+          if(think.isEmpty(shipperInfo))
+          {
+            return this.fail("快递信息错误");
+          }  
+
+          let orderExpressId = await this.model('order_express').where({order_id:id}).getField('id', true);;
+
+          if(think.isEmpty(orderExpressId)) // 不存在该快递信息，新增
+          {
+            let newInfo = {
+              order_id : id,
+              shipper_id : shipperInfo.id,
+              shipper_name : shipperInfo.name,
+              shipper_code : shipperInfo.code, 
+              logistic_code : values.expressInfo.logistic_code,
+              add_time : parseInt(new Date().getTime() / 1000),
+            };
+
+            this.model('order_express').add(newInfo);
+          }
+          else // 更新
+          {
+            let newInfo = {
+              id : orderExpressId,
+              shipper_id : shipperInfo.id,
+              shipper_name : shipperInfo.name,
+              shipper_code : shipperInfo.code, 
+              logistic_code : values.expressInfo.logistic_code
+            };
+            this.model('order_express').update(newInfo);
+          }
+        }
+      }
       await model.where({id: id}).update({order_status: parseInt(values.order_status)});
     }else{
       return this.fail("订单错误");
